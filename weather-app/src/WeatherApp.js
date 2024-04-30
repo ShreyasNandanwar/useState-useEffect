be353068203946b92b236d8311ae2cdddca77ec9
[Shreyas Nandanwar] <shreyasnandanwar0400@gmail.com>
2024-04-30 15:01:05 +0530

    added geolocation and search bar

--- weather-app/src/WeatherApp.js
+++ weather-app/src/WeatherApp.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react'
 import './WeatherApp.css';
 const WeatherApp = () => {
 
+  const [city, setCity] = useState('');
   const [weather, setWeather] = useState(null);
 
-  const fetchWeatherData = async () => {
+  const fetchWeatherbyCity = async (city) => {
     try {
-      const response = await fetch('http://api.weatherapi.com/v1/current.json?key=5dd80c3b220a4ab1a0a73615243004&q=London')
+      const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=5dd80c3b220a4ab1a0a73615243004&q=${city}`)
       if (!response.ok) {
         throw new Error('Failed to fetch weatherAPI');
       }
@@ -19,24 +20,70 @@ const WeatherApp = () => {
     }
   };
 
-  useEffect(() => {
-    fetchWeatherData();
-  }, []);
+  const fetchWeatherbyLocation = async (latitude, longitude) => {
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=5dd80c3b220a4ab1a0a73615243004&q=${latitude},${longitude}`);
+    if(!response.ok){
+      throw new Error('failed to fetch api');
+    }
 
+    const data = await response.json();
+    setWeather(data);
+  }
+
+  const getLocation = () => {
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition((position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        fetchWeatherbyLocation(latitude, longitude);
+      },(error) => {
+        console.error(error);
+      });
+    }else {
+      console.error(`geolocation is not supported by the browser`); 
+    }
+  };
+
+
+  useEffect(() => {
+    if(city){
+      fetchWeatherbyCity(city);
+    }
+    else{
+      getLocation();
+    }
+  }, [city]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(city){
+      fetchWeatherbyCity(city);
+    }
+  };
 
   return (
     <div className='main'>
       <h1>
         Weather App
       </h1>
-
+      <form
+        onSubmit={handleSubmit}
+      >
+        <input 
+        type='text'
+        value={city}
+        placeholder='Enter city name....'
+        onChange={(e) => setCity(e.target.value)}
+        />
+        <button type='submit'>Search</button>
+      </form>
       {weather ? (
         <div>
           <h2>{weather.location.name}</h2>
           <p>Temperature_C : {weather.current.temp_c}</p>
           <p>Temperature_F : {weather.current.temp_f}</p>
           <div className='condition'>
-            <img src={weather.current.condition.icon} />
+            <img src={weather.current.condition.icon} alt={weather.current.condition.text} />
             <p>{weather.current.condition.text}</p>
           </div>
         </div>
